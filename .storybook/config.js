d0e598faf0fd29b1f294a3e234673c342a991c23
ganderzz <stolen3@gmail.com>
2018-10-07 09:44:18 -0700

    Add storybook export to Github docs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
-  require('../stories/index.js');
+  require("../stories/index.jsx");
   // You can require as many stories as you need.
 }
 
